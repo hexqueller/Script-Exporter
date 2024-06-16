fbd8df50f0f5f11f5371a591b149a24dc621ddb5
V1RUSnya <dima.nesed@gmail.com>
2024-06-16 21:59:56 +0300

    Читаем YAML

--- exporter.go
+++ exporter.go
@@ -3,14 +3,46 @@ package main
 import (
 	"flag"
 	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
 
+type Config struct {
+	Jobs map[string]struct {
+		Name   string `yaml:"name"`
+		Cron   string `yaml:"cron"`
+		Script string `yaml:"script"`
+	} `yaml:"jobs"`
+}
+
 func main() {
-	port := flag.Int("p", 8080, "Port to listen on")
-	config := flag.String("c", "config.json", "Path to config file")
+	port := flag.Int("p", 9105, "Port to listen on")
+	configPath := flag.String("c", "default.yaml", "Path to config file")
 
 	flag.Parse()
 
-	fmt.Println(fmt.Sprintf("Listening on port %d", *port))
-	fmt.Println(fmt.Sprintf("Config: %s", *config))
+	fmt.Println(fmt.Sprintf("Listening on http://localhost:%d", *port))
+	fmt.Println(fmt.Sprintf("Config: %s", *configPath))
+	fmt.Println("")
+	// читаем файл YAML
+	data, err := ioutil.ReadFile(*configPath)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	// разбираем файл YAML в структуру Config
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	// выводим все полученные задания
+	for _, job := range config.Jobs {
+		fmt.Printf("Job: %s\n", job.Name)
+		fmt.Printf("Cron: %s\n", job.Cron)
+		fmt.Printf("Script: %s\n", job.Script)
+		fmt.Println()
+	}
 }
---
+++ go.mod
@@ -0,0 +1,7 @@
+module exporter.go
+
+go 1.22.4
+
+require (
+	gopkg.in/yaml.v2 v2.4.0
+)
