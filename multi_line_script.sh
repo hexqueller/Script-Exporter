8eed2fef4c8bf038f9e5ac4ca5968258f705869c
V1RUSnya <dima.nesed@gmail.com>
2024-06-18 00:47:27 +0300

    Почистил репозиторий

--- multi_line_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ARRAY_DISK=($(lsblk -b | grep ^sd[a-z][^0-9] | awk '{ print $1 }'))
-for element in "${ARRAY_DISK[@]}"
-do
-  size=($(lsblk -b | grep ^$element | awk '{ print $4 }'))
-  echo "node_exporter_disk_size_lsblk{disk=\"sdd\"} 100500"
-  echo "not_node_exporter_disk_size_lsblk{disk=\"sdc\"} 100600"
-  echo "node_exporter_disk_size_lsblk{disk=\"dsc\"} 15"
-done
